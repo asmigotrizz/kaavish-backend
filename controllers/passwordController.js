8d18961907bdc74bd07664420519dada8828379b
Asmad <asmadshoaib854@gmail.com>
2026-05-13 23:06:47 +0500

    fix gmail smtp port for Railway

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -5,7 +5,9 @@ const { pool } = require('../config/database');
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
